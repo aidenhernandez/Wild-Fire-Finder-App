1984c2122159f05328bdfe84eb9708e264cc506d
Adan <ahaiden21@gmail.com>
2020-12-06 23:14:23 -0800

    useState to set location in Map.js using a LocationInfoBox component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
 //defined constants
 const NATURAL_EVENT_WILDFIRE = 8;
 
 const Map = ({ eventData, center, zoom }) => {
+        const [locationInfo, setLocationInfo] = useState(null)
 
         const markers = eventData.map(ev => {
             if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
-                return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+                return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
             }
             return null
         })
@@ -22,6 +25,7 @@ const Map = ({ eventData, center, zoom }) => {
             >
                 {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
